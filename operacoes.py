80bf7c9c96c1f8c648f7dfeb1ac599d3b2326fe4
maquisaao <maxwpinheiro@gmail.com>
2025-08-17 18:34:10 -0300

    estudo de operaçoes

---
+++ operacoes.py
@@ -0,0 +1,30 @@
+# operacoes matematicas (basicas)
+
+# soma
+soma = 1 + 1
+print("soma:", soma)
+
+# subtracao
+subtracao = 1 - 1
+print("subtracao", subtracao)
+
+# multiplicacao
+multiplicacao = 1 * 1
+print("multiplicacao:", multiplicacao)  
+
+# divisao
+divisao = 1 / 1
+print("divisao:", divisao)  
+
+# potencia
+potencia = 1 ** 1
+print("potencia:", potencia)    
+
+# raiz quadrada
+import math
+raiz_quadrada = math.sqrt(1)
+print("raiz quadrada:", raiz_quadrada)  
+
+# resto da divisao
+resto_divisao = 1 % 1
+print("resto da divisao:", resto_divisao)
