5a03909d4dd241fc0e7bf1a000499ef8f5b17708
maquisaao <maxwpinheiro@gmail.com>
2025-08-18 08:56:39 -0300

    estudo de variaveis e tipos

---
+++ variaveis-e-tipos.py
@@ -0,0 +1,23 @@
+# variaveis e tipos
+a = 2
+b= 4
+c= 1.5
+d= "string"
+e= bool(True)
+f= bool(False)
+
+# numeros inteiros (int)
+soma = a + b
+print("int:", soma)
+
+# numeros de ponto flutuante (float)
+soma_float = a + c
+print("float:", soma_float)
+
+# textos (str)
+texto = d 
+print("eu sou uma", texto)
+
+# booleanos (bool)
+print("posso ser", e)
+print("ou posso ser", f)
