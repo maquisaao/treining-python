2db9960db248845cceee4ca3dec7c961bb8acbb2
maquisaao <maxwpinheiro@gmail.com>
2025-08-22 09:23:42 -0300

    exercicios1 e desativei autocompletar

---
+++ exercicios.py
@@ -0,0 +1,23 @@
+# 1 Faça um programa que receba a idade do usuário e diga se ele é maior ou menor de idade.   
+idade = int(input("Digite sua idade:"))
+if idade < 18:
+    print("Menor de idade")
+else:
+    print("Maior de idade")
+
+
+# 2 Faça um programa que receba duas notas digitadas pelo usuário. Se a nota for maior ou igual a seis, escreva aprovado, senão escreva reprovado.   
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota:"))
+media= (nota1 + nota2) / 2
+if media >= 6:
+    print("Aprovado")
+else:
+    print("Reprovado")
+
+
+# 3 Escreva um programa que resolva uma equação de segundo grau.   
+
+# 4 Escreva um programa que ordene uma lista numérica com três elementos.   
+
+# 5 Escreva um programa que receba dois números e um sinal, e faça a operação matemática definida pelo sinal.   
