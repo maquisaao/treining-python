f8b1726ae322eecd8654318b07ca70d978faf12c
maquisaao <maxwpinheiro@gmail.com>
2025-08-26 14:42:23 -0300

    enumerate

---
+++ funcoes_extras.py
@@ -0,0 +1,20 @@
+# LIST ENUMERATE
+
+x = [1, 2, 3, 4, 5]
+y = []
+
+for i in x: # metodo 'simples'
+    y.append(i**2)
+print(x)
+print(y)
+
+# y = [valor_a_adicionar laço condição] *usado list compregension*
+y2 = [i**2 for i in x] # retorna o quadrado da lista x
+print(y2)
+
+y3 = [i for i in x if i%2==0] # retorna os pares da lista x
+print(y3)
+
+y4 = [i for i in x if i%2==1] # retorna os impares da lista x
+print(y4)
+
